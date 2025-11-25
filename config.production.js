e7faa535f1308262ba58d22776fc9efb540c0973
MTI CONSULTING <mticonsulting59@gmail.com>
2025-11-25 23:44:01 +0100

    fix: Nouvelle URL backend avec CORS activé

--- config.production.js
+++ config.production.js
@@ -7,7 +7,7 @@ if (typeof window.CONFIG === 'undefined') {
     window.CONFIG = {
         GOOGLE_CLIENT_ID: '913475747202-dg6rnc0hhu16thk3gckbnqkdcoei2a1n.apps.googleusercontent.com',
         GOOGLE_CLIENT_SECRET: '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',
-        BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxTOqi84ohatIrRuZ12bb2GSPd__YnyqIKpO2Pz_YE78TdWjOTPv82gmOtQnF9w4GY_/exec',
+        BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyUp4uaDfbrZpziEXI3SRBYm8M_cF32mU17Ji_L3qYnxaQGl-K6KZ19-33yHkCCMD92/exec',
         SHEETS_ID: '1Zu6I-c64YrBdlfvWhiVnlbwbvhv6Mw5NL8iRn2mvXoE',
         CALENDAR_ID: '',  // Optionnel, sinon calendrier par défaut
         DRIVE_FOLDER: 'MTI_CONSULTING_DATA'
